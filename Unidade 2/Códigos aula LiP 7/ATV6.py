57a5b4cc500e120d67fabc3a0abb7cabea5ddc98
Matheus0820 <mr7052954@gmail.com>
2024-11-18 16:17:45 -0300

    feat: Update question 6 - LiP lab 7

--- Unidade 2/Códigos aula LiP 7/ATV6.py
+++ Unidade 2/Códigos aula LiP 7/ATV6.py
@@ -15,4 +15,21 @@ for i in range(n):
             list_num[i-1] = atualvalor
             list_num.append(aux)
 
-print(list_num)    
+        for j in range(len(list_num)):
+            for k in range(len(list_num) - 1):
+                if list_num[j] < list_num[k]:
+                    aux = list_num[j]
+                    list_num[j] = list_num[k]
+                    list_num[k] = aux
+
+if len(list_num)%2 != 0:
+    idc = int(len(list_num)/2)
+    print(f"idc: {idc}")
+    mediana = list_num[idc]
+else:
+    idc1 = int(len(list_num)/2) - 1
+    idc2 = idc1 + 1
+    mediana = (list_num[idc1] + list_num[idc2])/2
+    
+print(*list_num)
+print(f"Mediana: {mediana:.2f}")    
